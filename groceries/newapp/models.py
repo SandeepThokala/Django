375b86d52d3f27b5276cf3f6a96da9dcf6886109
SandeepThokala <imdeepu98@gmail.com>
2024-04-15 18:15:14 -0400

    migrations & items

--- groceries/newapp/models.py
+++ groceries/newapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=3, decimal_places=2)
+    quant = models.IntegerField()
+
+    def __str__(self):
+        return self.title
--- groceries/newapp/views.py
+++ groceries/newapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 def newapp(request):
-    return render(request, "newapp/newapp.html")
+    return render(request,
+                  "newapp/newapp.html",
+                  {'items': Item.objects.all()})
